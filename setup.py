5f11135979764886031e4cc0826b365eeeb20d1b
arcathrax <ivanjoelpollak@proton.me>
2025-11-11 09:20:36 +0100

    feat: add simple setup.py script

--- setup.py
+++ setup.py
@@ -1,20 +1,41 @@
-
 from setuptools import setup, find_packages
+import os
+
+# Read version from version module
+def get_version():
+    version_file = os.path.join(os.path.dirname(__file__), 'hbos-bluetooth-service', '_version.py')
+    with open(version_file, 'r') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return line.split('=')[1].strip().strip('"\'')
+    raise RuntimeError('Unable to find version string.')
+
+# Read requirements from requirements.txt
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+    requirements = [line.strip() for line in f.readlines() if line.strip() and not line.startswith("#")]
 
 setup(
     name="hbos-bluetooth-service",
-    version="0.1.0",
-    description="HBOS Bluetooth Service",
-    long_description="A service for managing Bluetooth devices and connections on HBOS.",
-    author="HBOS",
-    author_email="support@hbos.com",
+    version=get_version(),
+    description="HiFiBerry Bluetooth Service",
+    long_description="HiFiBerry Bluetooth Service, that manages all the incomming Bluetooth connections. "
+                     "The settings can be adjusted via the web interface or a config file."
+    author="HiFiBerry",
+    author_email="support@hifiberry.com",
     license="MIT",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=[],
+    packages=find_packages(),
+    install_requires=requirements,
+    data_files=[
+        ('/usr/lib/systemd/system', [
+            'systemd/hbos-bluetooth.service'
+        ]),
+        ('/usr/share/man/man1', [
+            'man/hbos-bluetooth.1'
+        ]),
+    ],
     entry_points={
         "console_scripts": [
-            "hbos-bluetooth = main:main",
+            "hbos-bluetooth=hbos-bluetooth-service.main:main",
         ],
     },
     classifiers=[
@@ -24,3 +45,5 @@ setup(
     ],
     python_requires=">=3.6",
 )
+
+
